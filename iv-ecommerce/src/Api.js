7c146de61f6ebda7a8a6a3c262aa62394b79a43b
wenjase <alecjohnson708@gmail.com>
2022-03-16 12:58:26 -0500

    work in process

---
+++ iv-ecommerce/src/Api.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+function Api(props) {
+    const URL = 'https://amazon24.p.rapidapi.com/api/category';
+
+    function Apiurl() {
+        const [api, setApi] = useState([])
+    }
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Api;
--- iv-ecommerce/src/App.js
+++ iv-ecommerce/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from '@material-ui/core';
 import NavBar from './components/NavBar';
+import Api from './Api';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Api />
       <Button variant="contained" color="primary">
       Hello World
     </Button>
